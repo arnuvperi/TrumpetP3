fbd3d577d13a05d2b1aea309ebe9ea23a3fbaa80
Arnuv Peri <arnuvperi@gmail.com>
2022-04-04 10:09:33 -0400

    synth working

--- arnuv-synthesizer.jl
+++ arnuv-synthesizer.jl
@@ -30,24 +30,15 @@ function generateTone(key, freq1::Float64, duration::Int64)
     return nothing
 end
 
-#define the two frequencies for each number tone in each row
-#found from analyzing spectrum of each tone in the sample file
-
-freq = [261.63,277.18,293.67,311.13,329.63,349.23,369.99,392,415.3,440,466.16,493.88,523.25,554.37,587.33,622.25,659.26,698.46,739.99,783.99,830.61,880,932.33,987.77,1046.5] 
-note = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6"]
-
-
 #regular notes
-regnote = GtkCssProvider(data="#end {color:red; background:white;}")
-wholenotes = ["C" 261.63; "D" 293.67; "E" 329.63; "F" 349.23; "G" 392.0; "A" 440.0; "B" 493.88; "C" 523.25]
-halfnotes = ["1/2" 261.63; "1/2" 293.67; "1/2" 329.63; "1/2" 349.23; "1/2" 392.0; "1/2" 440.0; "1/2" 493.88; "1/2" 523.25]
-quarternotes = ["1/4" 261.63; "1/4" 293.67; "1/4" 329.63; "1/4" 349.23; "1/4" 392.0; "1/4" 440.0; "1/4" 493.88; "1/4" 523.25]
+wholenotes = ["C" 261.63 1; "D" 293.67 3; "E" 329.63 5; "F" 349.23 6; "G" 392.0 8; "A" 440.0 10; "B" 493.88 12; "C" 523.25 13]
+halfnotes = ["1/2" 261.63 1; "1/2" 293.67 3; "1/2" 329.63 5; "1/2" 349.23 6; "1/2" 392.0 8; "1/2" 440.0 10; "1/2" 493.88 12; "1/2" 523.25 13]
+quarternotes = ["1/4" 261.63 1; "1/4" 293.67 3; "1/4" 329.63 5; "1/4" 349.23 6; "1/4" 392.0 8; "1/4" 440.0 10; "1/4" 493.88 12; "1/4" 523.25 13]
 
 #accidentals (sharps and flats)
-sharpnote = GtkCssProvider(data="#end {color:white; background:black;}")
-sharpwhole = ["C#" 277.18; "D#" 311.13; "F#" 369.99; "G#" 415.3; "A#" 466.16]
-sharphalf = ["1/2" 277.18; "1/2" 311.13; "1/2" 369.99; "G# 1/2" 415.3; "1/2" 466.16]
-sharpquarter = ["1/4" 277.18; "1/4" 311.13; "1/4" 369.99; "1/4" 415.3; "1/4" 466.16]
+sharpwhole = ["C#" 277.18 2; "D#" 311.13 4; "F#" 369.99 7; "G#" 415.3 9; "A#" 466.16 11]
+sharphalf = ["1/2" 277.18 2; "1/2" 311.13 4; "1/2" 369.99 7; "G# 1/2" 415.3 9; "1/2" 466.16 11]
+sharpquarter = ["1/4" 277.18 2; "1/4" 311.13 4; "1/4" 369.99 7; "1/4" 415.3 9; "1/4" 466.16 11]
 
 g = GtkGrid() # initialize a grid to hold buttons
 set_gtk_property!(g, :row_spacing, 5) # gaps between buttons
@@ -57,30 +48,85 @@ set_gtk_property!(g, :column_homogeneous, true)
 
 
 
+#regular notes
 for i in 1:size(wholenotes, 1) # add the row1 keys to the grid
-    key, freq1 = wholenotes[i,1:2]
+    
+    key, freq1, place = wholenotes[i,1:3]
     duration = 1
     b = GtkButton(key) # make a button for this key
     signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
-    push!(GAccessor.style_context(b), GtkStyleProvider(sharpnote), 600)
+    g[place, 4] = b  # put the button in row 3 of the grid
+
+    #color
+    regularNote = GtkCssProvider(data="#reg {color:black; background:white;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(regularNote), 600)
     set_gtk_property!(b, :name, key) 
-    g[i, 3] = b  # put the button in row 3 of the grid
 end
 
 for i in 1:size(halfnotes, 1) # add the row1 keys to the grid
-    key, freq1 = halfnotes[i,1:2]
+    key, freq1, place = halfnotes[i,1:3]
     duration = 2
     b = GtkButton(key) # make a button for this key
     signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
-    g[i, 2] = b  # put the button in row 2 of the grid
+    g[place, 3] = b  # put the button in row 2 of the grid
+
+    #color
+    regularNote = GtkCssProvider(data="#reg {color:black; background:white;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(regularNote), 600)
+    set_gtk_property!(b, :name, key) 
 end
 
 for i in 1:size(quarternotes, 1) # add the row1 keys to the grid
-    key, freq1 = quarternotes[i,1:2]
+    key, freq1, place = quarternotes[i,1:3]
     duration = 4
     b = GtkButton(key) # make a button for this key
     signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
-    g[i, 1] = b  # put the button in row 2 of the grid
+    g[place, 2] = b  # put the button in row 2 of the grid
+
+    #color
+    regularNote = GtkCssProvider(data="#reg {color:black; background:white;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(regularNote), 600)
+    set_gtk_property!(b, :name, key) 
+end
+
+#sharps
+for i in 1:size(sharpwhole, 1) # add the row1 keys to the grid
+    key, freq1, place = sharpwhole[i,1:3]
+    duration = 1
+    b = GtkButton(key) # make a button for this key
+    signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
+    g[place, 3] = b  # put the button in row 3 of the grid
+
+    #color
+    accidentalNote = GtkCssProvider(data="#acc {color:white; background:black;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(accidentalNote), 600)
+    set_gtk_property!(b, :name, key) 
+end
+
+for i in 1:size(sharphalf, 1) # add the row1 keys to the grid
+    key, freq1, place = sharphalf[i,1:3]
+    duration = 1
+    b = GtkButton(key) # make a button for this key
+    signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
+    g[place, 2] = b  # put the button in row 3 of the grid
+
+    #color
+    accidentalNote = GtkCssProvider(data="#acc {color:white; background:black;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(accidentalNote), 600)
+    set_gtk_property!(b, :name, key) 
+end
+
+for i in 1:size(sharpquarter, 1) # add the row1 keys to the grid
+    key, freq1, place = sharpquarter[i,1:3]
+    duration = 1
+    b = GtkButton(key) # make a button for this key
+    signal_connect((win) -> generateTone(key, freq1, duration), b, "clicked") # callback
+    g[place, 1] = b  # put the button in row 3 of the grid
+
+    #color
+    accidentalNote = GtkCssProvider(data="#acc {color:white; background:black;}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(accidentalNote), 600)
+    set_gtk_property!(b, :name, key) 
 end
 
 
@@ -151,6 +197,7 @@ signal_connect(win, "key-press-event") do widget, event # parse keyboard input
         undo_button_clicked(ubutton)
     end
 end
+
 
 cond = Condition()
 endit(win) = notify(cond)
