4aa9a27b4e579c452fad007db0200383de3ac5e1
Arnuv Peri <arnuvperi@gmail.com>
2022-04-12 00:02:19 -0400

    transcriber working

--- Arnuv GUI.jl
+++ Arnuv GUI.jl
@@ -2,6 +2,8 @@
 using Gtk
 using WAV: wavwrite
 using FFTW: fft
+using WAV: wavwrite, wavread
+using FFTW: fft, ifft
 using DelimitedFiles
 using Plots; default(markerstrokecolor=:auto, label="")
 using Statistics: mean
@@ -9,10 +11,14 @@ using Measures
 using Gtk: GtkGrid, GtkButton, GtkWindow, GAccessor
 using Gtk: GtkCssProvider, GtkStyleProvider
 using Gtk: set_gtk_property!, signal_connect, showall
+using Gtk: GtkGrid, GtkButton, GtkWindow, GAccessor, GtkCssProvider, GtkStyleProvider, set_gtk_property!, signal_connect, showall
 using PortAudio: PortAudioStream
 using Sound: record, sound
 using FFTW: fft, ifft
 using WAV: wavread
+
+
+##Initiliaze variables
 
 #Define GTK grid and properties
 g = GtkGrid() # initialize a grid to hold buttons
@@ -25,12 +31,25 @@ synthButtonModel = GtkCssProvider(data="#synth {color:white; background:green;}"
 transButtonModel = GtkCssProvider(data="#trans {color:white; background:blue;}")
 songSelection = GtkCssProvider(data="#songs {color:white; background:black;}")
 
+const S = 44100 # sampling rate (samples/second)
+const N = 1024 # buffer length
+const maxtime = 15 # maximum recording time 10 seconds (for demo)
+recording = nothing # flag
+nsample = 0 # count number of samples recorded
+song = nothing # initialize "song"
 
+inputSongFrequencies = []
+inputSongDurations = []
+inputSongMidi = []
 
-##READ DATABASE OF SONGS
+dataSongFrequencies = []
+dataSongDurations = []
+dataSongMidi = []
+
 filelist = readdir("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/Saved Songs")
 databaseSongs = []
 songNames = []
+
 for i in 1:size(filelist,1)
     if filelist[i][end-2:end] == "txt"
         push!(databaseSongs, filelist[i])##get text files of all songs
@@ -38,19 +57,7 @@ for i in 1:size(filelist,1)
     end
 end
 
-##Initiliaze variables
-
 currentSong = songNames[1]
-
-inputSongFrequencies = []
-inputSongDurations = []
-
-const S = 44100 # sampling rate (samples/second)
-const N = 1024 # buffer length
-const maxtime = 15 # maximum recording time 10 seconds (for demo)
-recording = nothing # flag
-nsample = 0 # count number of samples recorded
-song = nothing # initialize "song"
 
 for i in 1:size(songNames,1) # add the instrument buttons to the grid
     name = songNames[i]
@@ -59,42 +66,85 @@ for i in 1:size(songNames,1) # add the instrument buttons to the grid
     push!(GAccessor.style_context(b), GtkStyleProvider(songSelection), 600)
     set_gtk_property!(b, :name, "songs") # set style of the "instrument" button
     signal_connect((win) -> song_selected(i), b, "clicked") # callback
-    g[1:3, (1 + i) * 2] = b # put the button in column 1 of the grid
+    g[1:3, 1 + i] = b # put the button in column 1 of the grid
 end
 
 
 ##callback functions
+##generates record buttons
+function make_button(string, callback, column, stylename, styledata)
+    b = GtkButton(string)
+    signal_connect((w) -> callback(w), b, "clicked")
+    g[column,4] = b
+    s = GtkCssProvider(data = "#$stylename {$styledata}")
+    push!(GAccessor.style_context(b), GtkStyleProvider(s), 600)
+    set_gtk_property!(b, :name, stylename)
+    return b
+end
+
+##changes song selected
 function song_selected(index::Int64)
     global currentSong = songNames[index]
     GAccessor.text(songNameLabel, "Current Song: " * currentSong)
     println(currentSong * " selected")
+    #getSongData()
     plotSong()
+    g[5,1:4] = GtkImage("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/plot.png")
 end
 
-function plotSong()
+##get selected song data
+function getSongData()
     data = readdlm("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/Saved Songs/"* currentSong * ".txt")
-    frequencies = data[:,1] #extract frequencies
-    durations = data[:,2] #extract durations
+    global dataSongFrequencies = data[:,1] #extract frequencies
+    global dataSongDurations = data[:,2] #extract durations
+    global dataSongMidi = []
 
-    for i in 1:size(frequencies,1)
-        frequencies[i] = parse(Float64, frequencies[i][1:end-1])
+    for i in 1:size(dataSongFrequencies,1)
+        dataSongFrequencies[i] = parse(Float64, dataSongFrequencies[i][1:end-1])
+        midi = Int(round(12*log2(dataSongFrequencies[i]/440))+69)
+        push!(dataSongMidi, midi)
     end
-    generatePlot(durations, frequencies)
-    img = GtkImage("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/plot.png")
-    g[5,1] = img
+
+    display(dataSongMidi)
 end
 
-function generatePlot(durations::Vector{Any}, frequencies::Vector{Any})
-    midi_raw = [isnan(f) ? 63 : 69 + round(Int, 12 * log2(f/440)) for f in frequencies]
-    V = [-2 0 .5 .75 1 1.25 1.5 1.75 2 2.5 2.75 3 3.25 3.5 4 4.25 4.5]
-    v = V[midi_raw .- 62]
-    plt = plot(v, line=:stem, marker=:circle, markersize = 10, color=:black)
+
+##plots song for student to see
+function plotSong()
+
+    ##get selected song data
+    getSongData()
+
+    ##generate plot to place in upper right for student to see
+    generatePlot(dataSongDurations, dataSongMidi)
+
+    ##path name CHANGE AS NEEDED
+    img = GtkImage("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/plot.png")
+
+    ##place image in grid
+    g[5,1:4] = img
+end
+
+##generates stem plot
+function generatePlot(durations::Vector{Any}, midi::Vector{Any})
+    midiWithDuration = []
+    for i in 1:length(midi)-1
+
+        push!(midiWithDuration, midi[i])
+        num = floor(Int, durations[i] - 1.0)
+
+        for i in 1:num
+            push!(midiWithDuration, 0)
+        end
+    end
+    
+    plt = plot(midiWithDuration, line=:stem, marker=:circle, markersize = 10, color=:black)
     plot!(size = (800,200)) # size of plot
     plot!(widen=true) # try not to cut off the markers
-    plot!(xticks = [], ylims = (-0.7,4.7)) # for staff
-    yticks!(0:4, ["E", "G", "B", "D", "F"]) # helpful labels for staff lines
+    plot!(xticks = [], ylims = (60,85)) # for staff
+    yticks!([64,67,71,74,77,81,84], ["E", "G", "B", "D", "F", "A", "C"]) # helpful labels for staff lines
     plot!(yforeground_color_grid = :blue) # blue staff, just for fun
-    plot!(foreground_color_border = :white) # make border "invisible"
+    plot!(foreground_color_border = :red) # make border "invisible"
     plot!(gridlinewidth = 1.5) # increase width of grid lines
     plot!(gridalpha = 0.9) # make grid lines more visible
     plot!(margin = 5mm)
@@ -102,37 +152,97 @@ function generatePlot(durations::Vector{Any}, frequencies::Vector{Any})
     display(plt)
 end
 
-function autocorrelationCalculator(x::Vector{Float32})
-    S = 44100
-    x=x[1:2:end];S÷=2 # reduce memory
-    Nx= length(x)
-    
-    autocorr= real(ifft(abs2.(fft([x; zeros(length(x))]))))/ sum(abs2,x)
-    big1= autocorr .> 0.5 # find large values
-    big1[1:findfirst(==(false), big1)] .= false # ignore peak near m=0
-    
-    
-    peak2start= findfirst(==(true), big1)
-    peak2end = findnext(==(false), big1, peak2start) # end of 2nd peak
-    big1[peak2end:end] .= false # ignore everything to right of 2nd peak
+
+##autocorrelationCalculator for finding frequency of input PortAudio
+function autocorrelationCalculator()
+
+    ##fast autocorrelation method for small segment of time (1/16th note)
+    N = length(song)
+    t = 8192
+
+    for i in 1:t:(N-(t+1))
+        z = song[i:i+(t-1)]
+        n = length(z);
+
+        autocorr = real(ifft(abs2.(fft([z; zeros((n))])))) / sum(abs2, z) # normalize
+        big1 = autocorr .> 0.7 #Finds large values
+        big1[1:findfirst(==(false), big1)] .= false # ignore peak near m=0
+        peak2start = findfirst(==(true), big1)
+        peak2end = findnext(==(false), big1, peak2start) # end of 2nd peak
+        if isnothing(peak2end) #Skip peaks with nothing
+            continue
+        end
+        big1[peak2end:end] .= false # ignore everything to right of 2nd peak
+        m = argmax(big1 .* autocorr)-1
+        f = round(S/m, digits=2)
+
+        if f < 200
+            continue
+        end
+
+        push!(inputSongMidi, Int(round(12*log2(f/440))+69))
+    end
+
+    tempMidi = []
+    tempDuration = []
+    localDuration = 1.0
+
+
+    ##checks and adds for duration
+    for i in 1:length(inputSongMidi)-1
+        if inputSongMidi[i] == inputSongMidi[i+1]
+            localDuration = localDuration + 1.0;
+        elseif i == length(inputSongMidi)-1
+            push!(tempMidi, inputSongMidi[i])
+            push!(tempDuration, localDuration)
+        elseif inputSongMidi[i] != inputSongMidi[i+1]
+            push!(tempMidi, inputSongMidi[i])
+            push!(tempDuration, localDuration)
+            localDuration = 1.0
+        end
+    end
+
+    push!(tempMidi, inputSongMidi[end])
+    push!(tempDuration, localDuration)
+    global inputSongMidi = tempMidi
+    global inputSongDurations = tempDuration
     
 
-    m= argmax(big1 .* autocorr)-1
-    f = round(S/m, digits=2)
-    return f
+end
+
+function playMetronome()
+    bpm = 120
+    bps = bpm / 60 # beats per second
+    spb = 60 / bpm # seconds per beat
+    t0 = 0.01 # each "tick" is this long
+    tt = 0:1/S:15 # 9 seconds of ticking
+    x = randn(length(tt)) .* (mod.(tt, spb) .< t0) / 4.5 # click via "envelope"
+    sound(x, S)
 end
 
 
 ##finds areas where student was off for each 16th note
-function generateScore(inputSongFrequencies::Vector{Float64}, dataSongFrequencies::Vector{Float64})
+function generateScore()
+
+    ##error based on midi of note played
+    counter = 0
     errorLocations = []
-    for i in 1:size(dataSongFrequencies,1)
-        dataFreq = dataSongFrequencies[i]
-        inputFreq = inputSongFrequencies[i]
+    for i in 1:size(dataSongMidi,1)
+        counter = counter + 1
+        dataMidi = 0
+        inputMidi = 0
 
-        errorThreshold = 5
+        if i > length(inputSongMidi)
+            inputMidi = 0
+        else
+            inputMidi= inputSongMidi[i]
+            dataMidi = dataSongMidi[i]
+        end
 
-        percentError = (abs(inputFreq - dataFreq) / dataFreq) * 100
+        ##1 note off retruns an error
+        errorThreshold = 1
+
+        percentError = (abs(inputMidi - dataMidi) / dataMidi) * 100
 
         ##severity of error during each 16th note of playing
         if percentError >= 3 * errorThreshold
@@ -150,15 +260,7 @@ function generateScore(inputSongFrequencies::Vector{Float64}, dataSongFrequencie
 
 end
 
-
-
-# callbacks
-
-"""
-    record_loop!(in_stream, buf)
-Record from input stream until maximum duration is reached,
-or until the global "recording" flag becomes false.
-"""
+##record loop for taking in audio until end
 function record_loop!(in_stream, buf)
     global maxtime
     global S
@@ -190,8 +292,9 @@ function call_record(w)
     buf = read(in_stream, N) # warm-up
     global recording = true
     global song = zeros(Float32, maxtime * S)
-
+    
     @async record_loop!(in_stream, buf)
+    
     nothing
 end
 
@@ -216,19 +319,23 @@ function call_play(w)
     println("Play")
     @async sound(song, S) # play the entire recording
 
-    """
-    for i in 1:size(div(length(song), 8192),1) # add the instrument buttons to the grid
-        curSection = song[1+((i-1)*8192):8192*i]
-        f = autocorrelationCalculator(curSection)
-        push!(inputSongFrequencies, f)
-        push!(inputSongDurations, 1.0)
-    end
-    display(inputSongFrequencies)
+end
+
+# callback function for "play" button
+function call_transcribe(w)
+    println("Transcribe")
+    autocorrelationCalculator()
+
+    display(dataSongMidi)
+    display(inputSongMidi)
+
+    generateScore()
+
     """
     testData = [261.63;261.63;261.63;261.63;293.67;293.67;261.63;261.63;261.63;261.63;349.23;329.63;]
-
     testInput = [261.63;261.63;293.67;293.67;293.67;293.67;261.63;261.63;261.63;261.63;349.23;329.63;]
-    generateScore(testInput, testData)
+    generateScore()
+    """
 
 end
 
@@ -248,23 +355,14 @@ g[6:8, 5] = recordLabel
 img = GtkImage("/Users/arnuvperi/Library/CloudStorage/OneDrive-Personal/UMich/Winter 2022/ENGR 100/Project 3/Project 3 Code/TrumpetP3/plot.png")
 g[4:10,1:3] = img
 
-function make_button(string, callback, column, stylename, styledata)
-    b = GtkButton(string)
-    signal_connect((w) -> callback(w), b, "clicked")
-    g[column,4] = b
-    s = GtkCssProvider(data = "#$stylename {$styledata}")
-    push!(GAccessor.style_context(b), GtkStyleProvider(s), 600)
-    set_gtk_property!(b, :name, stylename)
-    return b
-end
 
 # create buttons with appropriate callbacks, positions, and styles
-br = make_button("Record", call_record, 6, "wr", "color:white; background:red;")
-bs = make_button("Stop", call_stop, 7, "yb", "color:yellow; background:blue;")
-bp = make_button("Play", call_play, 8, "wg", "color:white; background:green;")
+br = make_button("Record", call_record, 5, "wr", "color:white; background:red;")
+bs = make_button("Stop", call_stop, 6, "yb", "color:yellow; background:blue;")
+bp = make_button("Play Song", call_play, 7, "wg", "color:white; background:green;")
+bt = make_button("Generate Score", call_transcribe, 8, "wg", "color:white; background:purple;")
 
-
-
+getSongData()
 
 #Create window and push GTK to window
 win = GtkWindow("TrumpetScriber", 1000 , 600) # 1000×600 pixel window for all the buttons
